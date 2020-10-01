952c8eaddfa4b4ee2193bb12a7b9aca8b7116fe0
Farcy Corentin <corentinfarcy1@gmail.com>
2020-10-01 16:52:57 +0200

    scrip ajout

--- produits_ajout.php
+++ produits_ajout.php
@@ -3,50 +3,29 @@
        
         require "connexion_bdd.php"; // Inclusion de notrebibliothèque de fonctions
 
-        $db = connexionBase(); // Appel de la fonction deconnexion
-        $pro_id = $_GET["pro_id"];
-        $requete = "SELECT * FROM produits join categories on produits.pro_cat_id=categories.cat_id WHERE pro_id=".$pro_id;
-        $result = $db->query($requete);
-        if (!$result) 
-    {
-        $tableauErreurs = $db->errorInfo();
-        echo $tableauErreur[2]; 
-        die("Erreur dans la requête");
-    }
+        $db = connexionBase(); // Appel de la fonction deconnexion  
 
-    if ($result->rowCount() == 0) 
-    {
-       // Pas d'enregistrement
-       die("La table est vide");
-    }    
-        // Renvoi de l'enregistrement sous forme d'un objet
-        $produit = $result->fetch(PDO::FETCH_OBJ);
-
-        echo "<form action='script_modif.php' id='formulaire' method='POST'>
+        echo "<form action='script_ajout.php' id='formulaire' method='POST'>
         <div class='row'>
-        <div class='text-center col-12'> 
-        <img src='images/jarditou_photos/$produit->pro_id.jpg' class='img-fluid' alt='Image responsive'>
-        </div>
         <div class='col-12'>
             <div class='form-group'>
                 <label for='ref'>ID :</label>
-                <input type='text' readOnly  class='form-control' name='pro_id' id='pro_id' value='$produit->pro_id'>
+                <input type='text'  class='form-control' name='pro_id' id='pro_id' value=''>
                 <label for='ref'>Référence :</label>
-                <input type='text'  class='form-control' name='ref' id='ref' value='$produit->pro_ref'>
-                <label for='categorie'>Catégorie:  </label>
+                <input type='text'  class='form-control' name='ref' id='ref' value=''>
                 <div class='form-group'>                 
                 <label for='categorie'>Catégorie:  </label>                 
-                <input type='text' readOnly class='form-control' name='categorie' id='catégorie' placeholder='$produit->cat_nom'>
+                <input type='text' class='form-control' name='categorie' id='catégorie' value=''>
                 <label for='libelle'>Libellé :  </label>                 
-                <input type='text'  class='form-control' name='libelle' id='libelle' value='$produit->pro_libelle'>
+                <input type='text'  class='form-control' name='libelle' id='libelle' value=''>
                 <label for='description'>Description:  </label>                 
-                <input type='text'  class='form-control' name='description' id='description' value='$produit->pro_description'>
+                <input type='text'  class='form-control' name='description' id='description' value=''>
                 <label for='prix'>Prix:  </label>                 
-                <input type='text'  class='form-control' name='prix' id='prix' value='$produit->pro_prix'>
+                <input type='text'  class='form-control' name='prix' id='prix' value=''>
                 <label for='stock'>Stock:  </label>                 
-                <input type='text'  class='form-control' name='stock' id='stock' value='$produit->pro_stock'>
+                <input type='text'  class='form-control' name='stock' id='stock' value=''>
                 <label for='couleur'>Couleur:  </label>                 
-                <input type='text'  class='form-control' name='couleur' id='couleur' value='$produit->pro_couleur'>
+                <input type='text'  class='form-control' name='couleur' id='couleur' value=''>
                 <div class='form-group d-flex flex-column'> 
                 <div>
                 <label class='form-check-label' name='bloque' for='bloque'>Produit bloqué? :</label>
@@ -61,15 +40,15 @@
                 </div>
                 <div class='form-group'>
                 <label for='date_ajout'>Date d\'ajout :</label>
-                <input class='form-control'  type='date' name='date_ajout' id='date_ajout' value='$produit->pro_d_ajout'>
+                <input class='form-control'  type='date' name='date_ajout' id='date_ajout' value=''>
                 <label for='date_modif'>Date de modification :</label>
-                <input class='form-control'  type='date' name='date_modif' id='date_modif' value='$produit->pro_d_modif'>
+                <input class='form-control'  type='date' name='date_modif' id='date_modif' value=''>
                 </div> 
                 
                      
                 
                 
-                <a href='formulaire_modif.php?pro_id='$produit->pro_id' title='retour' role='button' class='btn btn-dark active mt-3 mb-2'>Retour</a>
+                <a href='tableau.php title='retour' role='button' class='btn btn-dark active mt-3 mb-2'>Retour</a>
                 <button type='submit' class='btn btn-warning mt-3 mb-2'>Envoyer</button>
                 <button type='reset' title='sup' class='btn btn-danger mt-3 mb-2'>Effacer</button>
                 </form>";      
--- script_modif.php
+++ script_modif.php
@@ -16,30 +16,20 @@ var_dump($_POST);
 
 
     $requete = $db->prepare("UPDATE produits 
-    SET pro_ref= ':pro_ref',pro_cat_id= :pro_cat_id,pro_photo='', pro_libelle= ':pro_libelle',pro_description= ':pro_description',pro_prix= :pro_prix,pro_stock= :pro_stock,pro_couleur= ':pro_couleur',pro_bloque= :pro_bloque,pro_d_ajout= ':pro_d_ajout',pro_d_modif= ':pro_d_modif'
+    SET pro_ref= :pro_ref,pro_cat_id= :pro_cat_id, pro_libelle= :pro_libelle,pro_description= :pro_description,pro_prix= :pro_prix,pro_stock= :pro_stock,pro_couleur= :pro_couleur,pro_bloque= :pro_bloque,pro_d_ajout= :pro_d_ajout,pro_d_modif= :pro_d_modif
     WHERE pro_id=:pro_id");
     $requete->bindValue(':pro_id',$pro_id,PDO::PARAM_INT);  
-    var_dump($pro_id);  
     $requete->bindValue(':pro_ref',$pro_ref);
-    var_dump($ref);
         $requete->bindValue(':pro_cat_id',$pro_cat_id,PDO::PARAM_INT);
-        var_dump($cat_id);
         $requete->bindValue(':pro_libelle',$pro_libelle);
-        var_dump($libelle);
         $requete->bindValue(':pro_description',$pro_description);
-        var_dump($description);
         $requete->bindValue(':pro_prix',$pro_prix,PDO::PARAM_INT);
-        var_dump($prix);
         $requete->bindValue(':pro_stock',$pro_stock,PDO::PARAM_INT);
-        var_dump($stock);
         $requete->bindValue(':pro_couleur',$pro_couleur);
-        var_dump($couleur);
         $requete->bindValue(':pro_bloque',$pro_bloque,PDO::PARAM_INT);
-        var_dump($bloque);
         $requete->bindValue(':pro_d_ajout',$pro_d_ajout);
-        var_dump($dateAjout);
         $requete->bindValue(':pro_d_modif',$pro_d_modif);
-        var_dump($dateModif);
+      
 
         $requete->execute();
         var_dump($requete);
